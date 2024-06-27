2d0e149e8638bc1dbab9603c138580dd5d2df8fd
kurtekat <142125482+kurtekat@users.noreply.github.com>
2024-06-27 14:56:16 -0400

    Update ServerTime.cpp

--- sdev/include/shaiya/src/ServerTime.cpp
+++ sdev/include/shaiya/src/ServerTime.cpp
@@ -39,13 +39,13 @@ Timestamp ServerTime::from_time_t(time_t tt)
     lt.wHour = time.hours().count();
     lt.wMinute = time.minutes().count();
     lt.wSecond = int(time.seconds().count());
-    return ServerTime::encoder(&lt);
+    return ServerTime::encoder(lt);
 }
 
 time_t ServerTime::to_time_t(Timestamp timestamp)
 {
     SYSTEMTIME lt{};
-    ServerTime::decoder(timestamp, &lt);
+    ServerTime::decoder(timestamp, lt);
 
     std::tm tm{};
     tm.tm_year = lt.wYear - 1900;
@@ -63,61 +63,61 @@ Timestamp ServerTime::now()
 {
     SYSTEMTIME lt{};
     GetLocalTime(&lt);
-    return ServerTime::encoder(&lt);
+    return ServerTime::encoder(lt);
 }
 
 // see ps_game.004E1CA0
-void ServerTime::decoder(Timestamp timestamp, LPSYSTEMTIME lt)
+void ServerTime::decoder(Timestamp timestamp, SYSTEMTIME& lt)
 {
     auto value = timestamp;
     value >>= 26;
     value &= 63;
     value += 2000;
-    lt->wYear = short(value);
+    lt.wYear = short(value);
 
     value = timestamp;
     value >>= 22;
     value &= 15;
-    lt->wMonth = short(value);
+    lt.wMonth = short(value);
 
     value = timestamp;
     value >>= 17;
     value &= 31;
-    lt->wDay = short(value);
+    lt.wDay = short(value);
 
     value = timestamp;
     value >>= 12;
     value &= 31;
-    lt->wHour = short(value);
+    lt.wHour = short(value);
 
     value = timestamp;
     value >>= 6;
     value &= 63;
-    lt->wMinute = short(value);
+    lt.wMinute = short(value);
 
     value = timestamp;
     value &= 63;
-    lt->wSecond = short(value);
+    lt.wSecond = short(value);
 }
 
 // see ps_game.004E1CF0
-Timestamp ServerTime::encoder(LPSYSTEMTIME lt)
+Timestamp ServerTime::encoder(const SYSTEMTIME& lt)
 {
-    Timestamp value = lt->wYear;
+    Timestamp value = lt.wYear;
     value -= 16;
     value <<= 4;
-    value += lt->wMonth;
+    value += lt.wMonth;
 
     value <<= 5;
-    value += lt->wDay;
+    value += lt.wDay;
 
     value <<= 5;
-    value += lt->wHour;
+    value += lt.wHour;
 
     value <<= 6;
-    value += lt->wMinute;
+    value += lt.wMinute;
 
     value <<= 6;
-    value += lt->wSecond;
+    value += lt.wSecond;
     return value;
 }
