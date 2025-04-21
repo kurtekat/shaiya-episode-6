c82700be8b678ce015f47bbb4920ce839f5e7f3d
kurtekat <142125482+kurtekat@users.noreply.github.com>
2025-04-20 20:43:01 -0400

    Update string_helper.h

--- util/string_helper.h
+++ util/string_helper.h
@@ -1,30 +1,63 @@
 #pragma once
-#include <sstream>
+#include <limits>
 #include <string>
 #include <vector>
 
 namespace util
 {
     /// <summary>
-    /// Splits a string into a maximum number of substrings based on a specified delimiting character.
+    /// Splits a string into a maximum number of substrings based on a specified delimiting string.
     /// </summary>
     template<class CharT>
-    std::vector<std::basic_string<CharT>> split(const std::basic_string<CharT>& str, CharT sep, size_t count = SIZE_MAX)
+    std::vector<std::basic_string<CharT>> split(const std::basic_string<CharT>& str, const std::basic_string<CharT>& sep, size_t count)
     {
         std::vector<std::basic_string<CharT>> tokens;
         if (!count)
             return tokens;
 
-        std::basic_istringstream<CharT, std::char_traits<CharT>, std::allocator<CharT>> iss(str);
         std::basic_string<CharT> token;
+        size_t last = 0;
+        size_t next = 0;
 
-        while (std::getline(iss, token, sep))
+        while ((next = str.find(sep, last)) != std::basic_string<CharT>::npos)
         {
+            token = str.substr(last, next - last);
             tokens.push_back(token);
+
             if (tokens.size() == count)
-                break;
+                return tokens;
+
+            last = next + sep.length();
         }
 
+        tokens.push_back(str.substr(last));
         return tokens;
+    }
+
+    /// <summary>
+    /// Splits a string into substrings based on a specified delimiting string.
+    /// </summary>
+    template<class CharT>
+    std::vector<std::basic_string<CharT>> split(const std::basic_string<CharT>& str, const std::basic_string<CharT>& sep)
+    {
+        return split(str, sep, std::numeric_limits<size_t>::max());
+    }
+
+    /// <summary>
+    /// Splits a string into a maximum number of substrings based on a specified delimiting character.
+    /// </summary>
+    template<class CharT>
+    std::vector<std::basic_string<CharT>> split(const std::basic_string<CharT>& str, CharT sep, size_t count)
+    {
+        return split(str, std::basic_string<CharT>(1, sep), count);
+    }
+
+    /// <summary>
+    /// Splits a string into substrings based on a specified delimiting character.
+    /// </summary>
+    template<class CharT>
+    std::vector<std::basic_string<CharT>> split(const std::basic_string<CharT>& str, CharT sep)
+    {
+        return split(str, std::basic_string<CharT>(1, sep), std::numeric_limits<size_t>::max());
     }
 }
