294d1010dc75fdf83a24e322b8ee6a028caf0718
kurtekat <142125482+kurtekat@users.noreply.github.com>
2025-05-30 20:37:37 -0400

    Update detour function

--- util/util.cpp
+++ util/util.cpp
@@ -1,27 +1,32 @@
-#include <string>
+#include <cstdint>
+#include <cstring>
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include "util.h"
 
-int util::detour(void* addr, void* func, size_t size)
+int util::detour(void* addr, void* dest, size_t size)
 {
-    constexpr int nop = 0x90;
-    constexpr int jmp = 0xE9;
-    auto dest = (unsigned(func) - unsigned(addr)) - 5;
+    #pragma pack(push, 1)
+    struct {
+        uint8_t opcode{ 0xE9 };
+        uint32_t operand;
+    } instruction{};
+    #pragma pack(pop)
 
-    if (size < 5)
+    static_assert(sizeof(instruction) == 5);
+
+    if (size < sizeof(instruction))
         return 0;
 
     unsigned long protect;
     if (!VirtualProtect(addr, size, PAGE_EXECUTE_READWRITE, &protect))
         return 0;
 
-    std::memset(addr, nop, size);
-    std::memset(addr, jmp, 1);
-    __asm { inc addr }
-    std::memcpy(addr, &dest, 4);
-    __asm { dec addr }
+    auto address = (size_t(dest) - size_t(addr)) - sizeof(instruction);
+    instruction.operand = address;
 
+    std::memset(addr, 0x90, size);
+    std::memcpy(addr, &instruction, sizeof(instruction));
     return VirtualProtect(addr, size, protect, &protect);
 }
 
--- util/util.h
+++ util/util.h
@@ -2,7 +2,7 @@
 
 namespace util
 {
-    int detour(void* addr, void* func, size_t size);
+    int detour(void* addr, void* dest, size_t size);
     int read_memory(void* addr, void* dest, size_t size);
     int write_memory(void* addr, void* src, size_t size);
     int write_memory(void* addr, int value, size_t size);
