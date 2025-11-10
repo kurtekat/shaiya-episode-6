f8532ac514f45759f72ec56bd66b70dd31bee48f
kurtekat <142125482+kurtekat@users.noreply.github.com>
2025-11-10 14:34:01 -0500

    Update models

---
+++ sdev-client/include/shaiya/ChatColor.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdint>
+
+namespace shaiya
+{
+    enum struct ChatColor : uint32_t
+    {
+        None,
+        LightBlue,
+        Blue,
+        Green,
+        Yellow,
+        Orange,
+        White,
+        Pink,
+        Purple,
+        LightPink,
+        Black
+    };
+}
