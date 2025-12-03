2c7a623de42a8d91a7b6811c64ed16484bcc691c
kurtekat <142125482+kurtekat@users.noreply.github.com>
2025-12-03 03:34:49 -0500

    Update filesystem.hpp

--- sdev/include/extensions/filesystem.hpp
+++ sdev/include/extensions/filesystem.hpp
@@ -5,14 +5,12 @@ namespace ext {
     namespace filesystem {
 
         template<class...Args>
-        inline std::filesystem::path& combine(std::filesystem::path& base, const Args&...args)
+        inline void combine(std::filesystem::path& base, const Args&...args)
         {
             for (const auto& path : std::initializer_list<std::filesystem::path>{ args... })
             {
                 base /= path;
             }
-
-            return base;
         }
 
     } // namespace filesystem
