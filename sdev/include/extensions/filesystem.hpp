6c5a403cff524c3b5a1427cb2cfbfd2f33cb23ae
kurtekat <142125482+kurtekat@users.noreply.github.com>
2025-12-31 13:59:26 -0500

    Refactor extensions namespaces

--- sdev/include/extensions/filesystem.hpp
+++ sdev/include/extensions/filesystem.hpp
@@ -1,21 +1,19 @@
 #pragma once
 #include <filesystem>
 
-namespace ext
-{
-    namespace filesystem
-    {
-        struct path
-        {
-            template<class...Args>
-            static void combine(std::filesystem::path& base, const Args&...args)
-            {
-                for (const auto& path : std::initializer_list<std::filesystem::path>{ args... })
-                {
-                    base /= path;
-                }
-            }
-        };
+namespace ext {
+namespace filesystem {
+namespace path {
 
-    } // namespace filesystem
+    template<class...Args>
+    inline void combine(std::filesystem::path& base, const Args&...args)
+    {
+        for (const auto& path : std::initializer_list<std::filesystem::path>{ args... })
+        {
+            base /= path;
+        }
+    }
+
+} // namespace path
+} // namespace filesystem
 } // namespace ext
--- sdev/include/extensions/ranges.hpp
+++ sdev/include/extensions/ranges.hpp
@@ -1,34 +1,32 @@
 #pragma once
 #include <ranges>
 
-namespace ext
-{
-    namespace ranges
+namespace ext {
+namespace ranges {
+namespace views {
+
+    template<std::ranges::viewable_range Range>
+    inline constexpr auto enumerate_n(Range&& range, std::ranges::range_difference_t<Range> count)
     {
-        namespace views
-        {
-            template<std::ranges::viewable_range Range>
-            inline constexpr auto enumerate_n(Range&& range, std::ranges::range_difference_t<Range> count)
-            {
-                return std::views::enumerate(range) | std::views::take(count);
-            }
+        return std::views::enumerate(range) | std::views::take(count);
+    }
 
-            template<class Container, std::ranges::viewable_range Range, class Pattern>
-            inline constexpr auto split(Range&& range, Pattern&& pattern)
-            {
-                return std::ranges::to<Container>(std::views::split(range, pattern));
-            }
+    template<class Container, std::ranges::viewable_range Range, class Pattern>
+    inline constexpr auto split(Range&& range, Pattern&& pattern)
+    {
+        return std::ranges::to<Container>(std::views::split(range, pattern));
+    }
 
-            template<class Container, std::ranges::viewable_range Range, class Pattern>
-            inline constexpr auto split(Range&& range, Pattern&& pattern, std::ranges::range_difference_t<Range> count)
-            {
-                return std::ranges::to<Container>(
-                    std::views::split(range, pattern) | std::views::take(count));
-            }
+    template<class Container, std::ranges::viewable_range Range, class Pattern>
+    inline constexpr auto split(Range&& range, Pattern&& pattern, std::ranges::range_difference_t<Range> count)
+    {
+        return std::ranges::to<Container>(
+            std::views::split(range, pattern) | std::views::take(count));
+    }
 
-        } // namespace views
-    } // namespace ranges
+} // namespace views
+} // namespace ranges
 
-    namespace views = ranges::views;
+namespace views = ranges::views;
 
 } // namespace ext
