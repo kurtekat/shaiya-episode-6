a0e1ac6ee2a293e30d803c731af7ef7c60a51d64
kurtekat <142125482+kurtekat@users.noreply.github.com>
2026-02-08 10:14:56 -0500

    Add case-insensitive key comparator

--- sdev/include/shaiya/Ini.h
+++ sdev/include/shaiya/Ini.h
@@ -2,6 +2,8 @@
 #include <filesystem>
 #include <map>
 #include <string>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 
 namespace shaiya
 {
@@ -20,7 +22,16 @@ namespace shaiya
 
     private:
 
-        std::map<std::wstring, std::wstring> m_data;
+        struct key_compare
+        {
+            bool operator()(const std::wstring& lhs, const std::wstring& rhs) const
+            {
+                return CompareStringOrdinal(
+                    lhs.data(), lhs.size(), rhs.data(), rhs.size(), true) == CSTR_LESS_THAN;
+            }
+        };
+
+        std::map<std::wstring, std::wstring, key_compare> m_data;
         std::filesystem::path m_path;
         std::wstring GetString(const wchar_t* section, const wchar_t* key, const wchar_t* defaultValue) const;
     };
