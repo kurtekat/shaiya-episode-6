9ca0c291fe5a2d1c89660e385b00a1b158a7839f
kurtekat <142125482+kurtekat@users.noreply.github.com>
2026-02-23 16:33:43 -0500

    Update MapPortal.h

--- sdev/include/shaiya/MapPortal.h
+++ sdev/include/shaiya/MapPortal.h
@@ -8,19 +8,24 @@ namespace shaiya
     {
         Neutral,
         Light,
-        Fury
+        Fury,
+        Invasion
     };
 
-    enum struct MapPortalInvasionCountry : uint16_t
+    enum struct BaseCountry : int32_t
     {
+        Neutral,
+        Light,
+        Fury,
+        // Invasion
         StableErdeLight = 1010,
         DeepDesertLight = 1011,
         StableErdeFury = 1020,
         DeepDesertFury = 1021,
         PalaionRegnumLight = 1060,
         ValdemarRegnumFury = 1061,
-        OpalusFury = 1065,
-        OpalusLight = 1066
+        OpalusLight = 1065,
+        OpalusFury = 1066
     };
 
     #pragma pack(push, 1)
@@ -29,7 +34,7 @@ namespace shaiya
         int32_t id;        //0x00
         SVector pos;       //0x04
         // 0x10
-        MapPortalCountry baseCountry;
+        BaseCountry baseCountry;
         // 0x14
         MapPortalCountry country;
         int32_t minLevel;  //0x18
