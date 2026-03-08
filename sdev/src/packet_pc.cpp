93e6821c3443fc4b4e29a90b1eb5db0fe621573b
kurtekat <142125482+kurtekat@users.noreply.github.com>
2026-03-08 15:31:41 -0400

    Move vehicle hooks to PacketPC source

--- sdev/src/packet_pc.cpp
+++ sdev/src/packet_pc.cpp
@@ -6,6 +6,7 @@
 #include "include/operator.h"
 #include "include/shaiya/CItem.h"
 #include "include/shaiya/CUser.h"
+#include "include/shaiya/CZone.h"
 #include "include/shaiya/ItemInfo.h"
 #include "include/shaiya/SConnection.h"
 #include "include/shaiya/UserHelper.h"
@@ -103,6 +104,38 @@ void hook_0x461005(CUser* user)
     SConnection::Send(user, &outgoing, sizeof(GameUserStatusOutgoing));
 }
 
+/// <summary>
+/// Sends packet 0x51D (6.4) to the zone.
+/// </summary>
+void hook_0x491490(CUser* user, GameCharShapeOutgoing_EP5* packet)
+{
+    GameCharShapeOutgoing_EP6_4 outgoing{};
+    outgoing.objectId = user->id;
+    outgoing.shapeType = packet->shapeType;
+    auto& vehicle = user->inventory[0][13];
+    outgoing.vehicleType = !vehicle ? 0 : vehicle->type;
+    outgoing.vehicleTypeId = !vehicle ? 0 : vehicle->typeId;
+
+    if (!user->zone)
+        return;
+
+    CZone::SendView(user->zone, &outgoing, sizeof(GameCharShapeOutgoing_EP6_4), user->cellX, user->cellZ);
+}
+
+/// <summary>
+/// Sends packet 0x51D (6.4) to the target.
+/// </summary>
+void hook_0x42A56C(CUser* target, CUser* user, ShapeType shapeType)
+{
+    GameCharShapeOutgoing_EP6_4 outgoing{};
+    outgoing.objectId = user->id;
+    outgoing.shapeType = shapeType;
+    auto& vehicle = user->inventory[0][13];
+    outgoing.vehicleType = !vehicle ? 0 : vehicle->type;
+    outgoing.vehicleTypeId = !vehicle ? 0 : vehicle->typeId;
+    SConnection::Send(target, &outgoing, sizeof(GameCharShapeOutgoing_EP6_4));
+}
+
 unsigned u0x4784DB = 0x4784DB;
 unsigned u0x479155 = 0x479155;
 void __declspec(naked) naked_0x4784D6()
@@ -147,13 +180,88 @@ void __declspec(naked) naked_0x461005()
     }
 }
 
-double g_itemRepairBuyDivisor = 15.0;
+unsigned u0x4914A9 = 0x4914A9;
+void __declspec(naked) naked_0x491490()
+{
+    __asm
+    {
+        pushad
+
+        lea eax,[esp+0x20]
+
+        push eax // packet
+        push ecx // user
+        call hook_0x491490
+        add esp,0x8
+        
+        popad
+
+        jmp u0x4914A9
+    }
+}
+
+unsigned u0x42A57E = 0x42A57E;
+void __declspec(naked) naked_0x42A56C()
+{
+    __asm
+    {
+        pushad
+
+        push eax // shapeType
+        push esi // user
+        push ebx // target
+        call hook_0x42A56C
+        add esp,0xC
+        
+        popad
+
+        jmp u0x42A57E
+    }
+}
+
+unsigned u0x4263B8 = 0x4263B8;
+void __declspec(naked) naked_0x4263AD()
+{
+    // edi = CUser*
+    __asm
+    {
+        // shapeType
+        mov byte ptr[esp+0x3E],al
+
+        xor eax,eax
+        mov [esp+0x3F],eax
+        mov [esp+0x43],eax
+
+        // user->inventory[0][13]
+        mov eax,[edi+0x1F4]
+        test eax,eax
+        je without_vehicle
+
+        // vehicle->type
+        movzx ecx,byte ptr[eax+0x40]
+        mov [esp+0x3F],ecx
+
+        // vehicle->typeId
+        movzx ecx,byte ptr[eax+0x41]
+        mov [esp+0x43],ecx
+
+        without_vehicle:
+
+        lea ecx,[esp+0x38]
+        push 0xF // length
+        push ecx // packet
+
+        jmp u0x4263B8
+    }
+}
+
+double g_repairDivisor = 15.0;
 unsigned u0x471C29 = 0x471C29;
 void __declspec(naked) naked_0x471C23()
 {
     __asm
     {
-        fld qword ptr[g_itemRepairBuyDivisor]
+        fld qword ptr[g_repairDivisor]
         jmp u0x471C29
     }
 }
@@ -163,7 +271,7 @@ void __declspec(naked) naked_0x47202A()
 {
     __asm
     {
-        fdiv qword ptr[g_itemRepairBuyDivisor]
+        fdiv qword ptr[g_repairDivisor]
         jmp u0x472030
     }
 }
@@ -174,6 +282,12 @@ void hook::packet_pc()
     util::detour((void*)0x4784D6, naked_0x4784D6, 5);
     // CUser::SetAttack
     util::detour((void*)0x461005, naked_0x461005, 5);
+    // CUser::SendShape
+    util::detour((void*)0x491490, naked_0x491490, 6);
+    // CCell::SendCellInfoEnter
+    util::detour((void*)0x42A56C, naked_0x42A56C, 6);
+    // CZone::SendMoveUser
+    util::detour((void*)0x4263AD, naked_0x4263AD, 6);
     // CUser::ItemRepair
     util::detour((void*)0x471C23, naked_0x471C23, 6);
     util::detour((void*)0x47202A, naked_0x47202A, 6);
--- sdev/src/user_shape.cpp
+++ sdev/src/user_shape.cpp
@@ -1,13 +1,10 @@
 #include <util/util.h>
 #include <shaiya/include/common/ShapeType.h>
 #include <shaiya/include/network/game/outgoing/0300.h>
-#include <shaiya/include/network/game/outgoing/0500.h>
 #include "include/main.h"
 #include "include/shaiya/CItem.h"
 #include "include/shaiya/CloneUser.h"
 #include "include/shaiya/CUser.h"
-#include "include/shaiya/CZone.h"
-#include "include/shaiya/SConnection.h"
 using namespace shaiya;
 
 void hook_0x45A365(CUser* user, CUser* target)
@@ -60,38 +57,6 @@ void hook_0x45A365(CUser* user, CUser* target)
     }
 }
 
-/// <summary>
-/// Sends packet 0x51D (6.4) to the zone.
-/// </summary>
-void hook_0x491490(CUser* user, GameCharShapeOutgoing_EP5* packet)
-{
-    GameCharShapeOutgoing_EP6_4 outgoing{};
-    outgoing.objectId = user->id;
-    outgoing.shapeType = packet->shapeType;
-    auto& vehicle = user->inventory[0][13];
-    outgoing.vehicleType = !vehicle ? 0 : vehicle->type;
-    outgoing.vehicleTypeId = !vehicle ? 0 : vehicle->typeId;
-
-    if (!user->zone)
-        return;
-
-    CZone::SendView(user->zone, &outgoing, sizeof(GameCharShapeOutgoing_EP6_4), user->cellX, user->cellZ);
-}
-
-/// <summary>
-/// Sends packet 0x51D (6.4) to the target.
-/// </summary>
-void hook_0x42A56C(CUser* target, CUser* user, ShapeType shapeType)
-{
-    GameCharShapeOutgoing_EP6_4 outgoing{};
-    outgoing.objectId = user->id;
-    outgoing.shapeType = shapeType;
-    auto& vehicle = user->inventory[0][13];
-    outgoing.vehicleType = !vehicle ? 0 : vehicle->type;
-    outgoing.vehicleTypeId = !vehicle ? 0 : vehicle->typeId;
-    SConnection::Send(target, &outgoing, sizeof(GameCharShapeOutgoing_EP6_4));
-}
-
 unsigned u0x45A246 = 0x45A246;
 void __declspec(naked) naked_0x45A365()
 {
@@ -107,81 +72,6 @@ void __declspec(naked) naked_0x45A365()
         popad
 
         jmp u0x45A246
-    }
-}
-
-unsigned u0x4914A9 = 0x4914A9;
-void __declspec(naked) naked_0x491490()
-{
-    __asm
-    {
-        pushad
-
-        lea eax,[esp+0x20]
-
-        push eax // packet
-        push ecx // user
-        call hook_0x491490
-        add esp,0x8
-        
-        popad
-
-        jmp u0x4914A9
-    }
-}
-
-unsigned u0x42A57E = 0x42A57E;
-void __declspec(naked) naked_0x42A56C()
-{
-    __asm
-    {
-        pushad
-
-        push eax // shapeType
-        push esi // user
-        push ebx // target
-        call hook_0x42A56C
-        add esp,0xC
-        
-        popad
-
-        jmp u0x42A57E
-    }
-}
-
-unsigned u0x4263B8 = 0x4263B8;
-void __declspec(naked) naked_0x4263AD()
-{
-    // edi = CUser*
-    __asm
-    {
-        // shapeType
-        mov byte ptr[esp+0x3E],al
-
-        xor eax,eax
-        mov [esp+0x3F],eax
-        mov [esp+0x43],eax
-
-        // user->inventory[0][13]
-        mov eax,[edi+0x1F4]
-        test eax,eax
-        je without_vehicle
-
-        // vehicle->type
-        movzx ecx,byte ptr[eax+0x40]
-        mov [esp+0x3F],ecx
-
-        // vehicle->typeId
-        movzx ecx,byte ptr[eax+0x41]
-        mov [esp+0x43],ecx
-
-        without_vehicle:
-
-        lea ecx,[esp+0x38]
-        push 0xF // length
-        push ecx // packet
-
-        jmp u0x4263B8
     }
 }
 
@@ -201,12 +91,6 @@ void hook::user_shape()
 {
     // CUser::CheckTargetUser case 2
     util::detour((void*)0x45A365, naked_0x45A365, 7);
-    // CUser::SendShape
-    util::detour((void*)0x491490, naked_0x491490, 6);
-    // CCell::SendCellInfoEnter
-    util::detour((void*)0x42A56C, naked_0x42A56C, 6);
-    // CZone::SendMoveUser
-    util::detour((void*)0x4263AD, naked_0x4263AD, 6);
     // CObjectMgr::CreateCloneUser
     util::detour((void*)0x411B1F, naked_0x411B1F, 7);
 }
